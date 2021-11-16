233d952dc7f17af1769cd39f8b92f2664cef069f
smileD <476700530@qq.com>
2021-11-17 01:29:21 +0800

    配置全局跨域

---
+++ src/main/java/com/ddpzp/xiaogu_word/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ddpzp.xiaogu_word.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 配置全局跨域
+ * Created by dd
+ * Date 2021/11/17 0:42
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
